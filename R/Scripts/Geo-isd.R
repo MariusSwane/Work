a72060611e591cf49f72e9de7fdce0ef20682926
Marius Swane <marius.wishman@ntnu.no>
2021-06-17 14:24:48 +0200

    Thu Jun 17 02:24:48 PM CEST 2021

--- R/Scripts/Geo-isd.R
+++ R/Scripts/Geo-isd.R
@@ -26,6 +26,7 @@ rm(list = ls())
 #	Loading packages 						       # 
 #==============================================================================#
 
+library(conflicted)
 library(dplyr)
 library(ggplot2)
 library(priogrid)
@@ -40,10 +41,15 @@ library(spData)
 library(tidyr)
 
 #==============================================================================#
-#	Loading data  							       #
+#	Setting preferences                                                    #
 #==============================================================================#
 
-isd <- read.csv2('../Data/ISDV2_Africa.csv')
+conflict_prefer("filter", "dplyr")
+conflict_prefer("last", "dplyr")  
+
+#==============================================================================#
+#	Loading data  							       #
+#==============================================================================#
 
 geoisd <- st_read('../../QGIS/Geo-ISD.shp')
 
@@ -96,23 +102,24 @@ geoisd_borders_data <- read_csv('../../QGIS/Geo-ISD_Borders2.csv',
 prio_grid_shp <- st_read('../Data/PRIO-Grid/priogrid_cell.shp')
 
 prio_grid <- read_csv('../Data/PRIO-Grid/priogridyv50-10.csv') %>% 
-  filter( (gwno >= 404 & gwno <= 626) | gwno == 651) %>% 
+  as_tibble() %>% filter( (gwno >= 404 & gwno <= 626) | gwno == 651) %>% 
   group_by(gid) %>% 
-  summarise(gid = gid, bdist3 = mean(bdist3),
-  capdist = mean(capdist), excluded = mean(excluded), temp = mean(temp), 
-  	  temp_sd = sd(temp), gwno = gwno,
+  summarise(bdist3 = mean(bdist3),
+  capdist = mean(capdist), excluded = mean(excluded), 
+  	  temp_sd = sd(temp), gwno = last(gwno), temp = mean(temp), 
 	  prec_sd = sd(prec_gpcc, na.rm = TRUE), prec_gpcc = mean(prec_gpcc))
-
-head(prio_grid)
-
-# TODO: Fix the sd of prec_gpcc and temp
 
 prio_grid_static  <- read_csv('../Data/PRIO-Grid/PRIO-GRID Static Variables - 2021-06-04.csv') 
 
-prio_grid_merge  <- left_join(prio_grid, prio_grid_static, by = c("gid")) 
+# Merging static and aggregated prio data
+prio_grid  <- left_join(prio_grid, prio_grid_static, by = c("gid")) 
 
-prio_grid_merge <- left_join(prio_grid_shp, prio_grid_merge, by = c("gid")) %>% 
+# Merging with the grid shape 
+prio_grid <- left_join(prio_grid_shp, prio_grid, by = c("gid")) %>% 
   filter( (gwno >= 404 & gwno <= 626) | gwno == 651)
+
+# Cleaning
+rm(prio_grid_static, prio_grid_shp)
 
 # Interpolated years from atlas maps
 geoisd_interpol <- geoisd_data %>% filter(is.na(year) == T & is.na(lyear) == F) %>%
@@ -150,6 +157,8 @@ borders_interpol <- borders_interpol %>%
 #  Checking for naming errors  						       #
 #==============================================================================#
 
+#isd <- read.csv2('../Data/ISDV2_Africa.csv')
+
 #isd$isdcow <- paste(isd$COWNum, isd$COWID)
 #
 #geoisd$isdcow <- paste(geoisd$COWID, geoisd$ISDID)
@@ -180,7 +189,7 @@ geoisd_data_gid <- geoisd_data_gid_year %>%
 
 # State presence with interpolated years from atlas maps
 
-main_data <- geoisd_interpol %>% 
+geoisd_data_gid_int <- geoisd_interpol %>% 
   mutate(state_presence = 1) %>%
   group_by(gid, year) %>%
   summarise(state_presence_max = max(na.omit(state_presence)),
@@ -188,7 +197,7 @@ main_data <- geoisd_interpol %>%
             state_presence_all = sum(na.omit(state_presence))
   )
 
-geoisd_data_gid_int <- main_data %>% 
+geoisd_data_gid_int <- geoisd_data_gid_int %>% 
   group_by(gid) %>%
             # Summing years with any state presence
   summarise(sp_i_sum_any = sum(na.omit(state_presence_max)),
@@ -241,7 +250,7 @@ overlap <- overlap %>%
 
 # Overlapping sovereignty (interpolated)
 
-overlap_int <- main_data %>% 
+overlap_int <- geoisd_data_gid_int %>% 
   group_by(gid, year) %>%
   summarise(overlap_int = max(na.omit(no_unique_states)))-1
 
@@ -291,7 +300,7 @@ geoisd_one_state_int <- geoisd_interpol %>%
 #	Merge with PRIOGRID  						       #
 #==============================================================================#
 
-prio_grid_isd <- left_join(prio_grid_merge, geoisd_data_gid, by = c("gid")) %>%
+prio_grid_isd <- left_join(prio_grid, geoisd_data_gid, by = c("gid")) %>%
   mutate(sp_sum=ifelse(is.na(sp_sum) == T, 0, sp_sum),
          sp_sum_any=ifelse(is.na(sp_sum_any) == T, 0, sp_sum_any))
 
@@ -320,6 +329,14 @@ prio_grid_isd <- left_join(prio_grid_isd, geoisd_one_state, by = c("gid")) %>%
 prio_grid_isd <- left_join(prio_grid_isd, geoisd_one_state_int, by = c("gid")) %>%
   mutate(sp_os_i_sum=ifelse(is.na(sp_os_i_sum) == T, 0, sp_os_i_sum),
          sp_os_i_sum_any=ifelse(is.na(sp_os_i_sum_any) == T, 0, sp_os_i_sum_any))
+
+#==============================================================================#
+#	Cleaning							       #
+#==============================================================================#		
+
+rm(geoisd, geoisd_data, geoisd_interpol, prio_grid, geoisd_one_state_int,
+   geoisd_one_state, overlap_int, overlap, borders_int_summed, borders_summed,
+   geoisd_data_gid_int, geoisd_data_gid)
 
 #==============================================================================#
 #	Plotting state presence  					       #
@@ -382,17 +399,6 @@ for (i in 1:length(plot_list)) {
 }
 
 
-# State Presence (with interpolations) colored by state between 1800-1914
-pal <- colorRampPalette(brewer.pal(82, name = "Set3"))
-ggplot() +
-  geom_sf(data = prio_grid_merge) +
-  geom_sf(data = geoisd,
-          linetype = 0,
-          aes(fill = as.factor(COWID), alpha = .1), 
-          show.legend = FALSE) + 
-  scale_fill_manual(values=sample(pal(82))) +
-  theme_minimal()
-
 #==============================================================================#
 #	Loading coast data						       #
 #==============================================================================#
@@ -407,6 +413,9 @@ coastline <- sf::st_boundary(coastline)
 
 prio_grid_isd$distcoast <- get_closest_distance(prio_grid_isd, coastline)
 
+# Cleaning
+rm(coastline)
+
 #==============================================================================#
 #	Plotting to see it looks right					       #
 #==============================================================================#
@@ -420,9 +429,9 @@ ggplot() +
     theme_minimal()
 
 ggplot() +
-	geom_sf(data = pggisd,
+	geom_sf(data = prio_grid_isd,
             linetype = 0,
-            aes_string(fill = temp)),
+            aes_string(fill = "non_state"),
             show.legend = FALSE) + 
     scale_fill_viridis_c() +
     theme_minimal()
@@ -441,17 +450,20 @@ popdr  <- raster_to_pg(popdr, aggregation_function = "mean")
 popdr  <- raster_to_tibble(popdr, add_pg_index = TRUE)
 
 # Tidying -- not working
-# popdr  <- popdr %>% rename(popd = layer, gid = pgid) %>% select(popd, gid)
+popdr  <- popdr %>% rename(popd = layer, gid = pgid) %>% dplyr::select(popd, gid)
 
 # Merging
-pggisd  <- left_join(prio_grid_isd, popdr, by = c("gid"))
+prio_grid_isd <- left_join(prio_grid_isd, popdr, by = c("gid"))
+
+# Cleaning
+rm(popdr)
 
 #==============================================================================#
 #	Afrobarometer							       #
 #==============================================================================#
 
 # Loading afrobarometer data
-afroba  <- revd.csv("../Data/afb_full_r3.csv")
+afroba  <- read.csv("../Data/afb_full_r3.csv")
 
 afroba  <- st_as_sf(afroba, coords = c("longitude", "latitude"))
 
@@ -459,3 +471,27 @@ afroba  <- st_as_sf(afroba, coords = c("longitude", "latitude"))
 
 vector_to_pg(afro, mean, need_aggregation = FALSE, missval = -99)
 
+#==============================================================================#
+#	UCDP Georeferenced Event Dataset (GED)				       #
+#==============================================================================#
+
+# Loading data
+load("../Data/ged201.RData")
+
+# Summarising
+ged  <- ged201 %>% group_by(priogrid_gid) %>% 
+	summarise(state_based = sum(type_of_violence == 1),
+	non_state = sum(type_of_violence == 2),
+	one_sided = sum(type_of_violence == 3))
+
+# Merging
+prio_grid_isd  <- left_join(prio_grid_isd, ged, by = c("gid" = "priogrid_gid"))
+
+# NA's should be 0 for conflict events
+prio_grid_isd  <- prio_grid_isd %>% 
+	mutate(state_based = ifelse(is.na(state_based), 0, state_based)) %>% 
+	mutate(non_state = ifelse(is.na(non_state), 0, non_state)) %>%  
+	mutate(one_sided = ifelse(is.na(one_sided), 0, one_sided))
+
+# Cleaning
+rm(ged, ged201)

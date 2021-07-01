ba0b509c44ff2a55893ffae3b1abd33bc3b2636d
Marius Swane <marius.wishman@ntnu.no>
2021-07-01 15:18:26 +0200

    Thu Jul  1 03:18:26 PM CEST 2021

--- R/Scripts/AnalysisGeoISD.R
+++ R/Scripts/AnalysisGeoISD.R
@@ -23,6 +23,8 @@ library(lemon)
 library(ggrepel)
 library(ggeffects)
 library(splines)
+library(conflicted)
+library(MASS)
 
 #==============================================================================#
 #	Loading Data							       #
@@ -35,6 +37,14 @@ load("../Data/GeoISDControls.Rdata")
 #==============================================================================#
 
 prio_grid_isd <- prio_grid_isd %>% mutate(
+				sqrtDeaths = sqrt(deaths),
+				sqrtState_based = sqrt(state_based),
+				sqrtPpp = sqrt(gcp_ppp),
+				sqrtSpAll = sqrt(sp_os_i_sum),
+				sqrtSpAny = sqrt(sp_os_i_sum_any),
+				sqrtCapdist = sqrt(capdist),
+				sqrtBDist = sqrt(bdist3),
+				sqrtPopd = sqrt(popd),
 				logDeaths = log(deaths + 1),
 				logState_based = log(state_based + 1),
 				logPpp = log(gcp_ppp + 1),
@@ -43,6 +53,8 @@ prio_grid_isd <- prio_grid_isd %>% mutate(
 				logCapdist = log(capdist + 1),
 				logBDist = log(bdist3 + 1),
 				logPopd = log(popd + 1),
+				sqrtorg3 = sqrt(org3),
+				sqrtNonstate = sqrt(non_state),
 				SpAllXCapDist = logSpAll*capdist,
 				SpAnyXCapDist = logSpAny*capdist)
 
@@ -54,11 +66,8 @@ GeoISDanalysis <- function(dvs, ivs, controls, data, test_label){
   models_out_j <- NULL
   models_out <- NULL
   margins_out <- NULL
-  
   for(i in 1:length(dvs)) {
-    
     dv <- dvs[i] 
-    
     for(j in 1:length(ivs)) {
       m1 <- lm(as.formula(paste(dv, '~', ivs[j], controls)),
                data=data)
@@ -66,7 +75,6 @@ GeoISDanalysis <- function(dvs, ivs, controls, data, test_label){
                data=data)
       m3 <- lm(as.formula(paste(dv, '~', ivs[j], controls, extended_controls, 
 				all_controls)), data=data)
-      
       iv_margins <- ivs[j] %>% strsplit(., "\\+") %>% unlist()
       m1_m <- summary(margins(m1, variables=iv_margins, change = "sd")) %>% 
         mutate(test='Baseline model', dep_var=dv)
@@ -74,9 +82,38 @@ GeoISDanalysis <- function(dvs, ivs, controls, data, test_label){
         mutate(test='Extended controls', dep_var=dv)
       m3_m <- summary(margins(m3, variables=iv_margins, change = "sd")) %>% 
         mutate(test='Full model', dep_var=dv)
-
-      margins_out <- bind_rows(margins_out, m1_m, m2_m,m3_m, m3_m)
+      margins_out <- bind_rows(margins_out, m1_m, m2_m, m3_m)
       models_out_j[[j]] <- list(m1, m2, m3)
+    } 
+    models_out[[i]] <- models_out_j %>% purrr::flatten()
+  }
+  models_out <- models_out
+  final <- list('margins'=margins_out, 'models'=models_out)
+  return(final)
+}
+
+GeoISD_NBanalysis <- function(dvs, ivs, controls, data, test_label){
+  models_out_j <- NULL
+  models_out <- NULL
+  margins_out <- NULL
+  for(i in 1:length(dvs)) {
+    dv <- dvs[i] 
+    for(j in 1:length(ivs)) {
+#      m1 <- glm.nb(as.formula(paste(dv, '~', ivs[j], controls)),
+#               data=data)
+#      m2 <- glm.nb(as.formula(paste(dv, '~', ivs[j], controls, extended_controls)),
+#               data=data)
+      m3 <- glm.nb(as.formula(paste(dv, '~', ivs[j], controls, extended_controls, 
+				all_controls)), data=data)
+      iv_margins <- ivs[j] %>% strsplit(., "\\+") %>% unlist()
+#      m1_m <- summary(margins(m1, variables=iv_margins, change = "sd")) %>% 
+#        mutate(test='Baseline model', dep_var=dv)
+#      m2_m <- summary(margins(m2, variables=iv_margins, change = "sd")) %>% 
+#        mutate(test='Extended controls', dep_var=dv)
+      m3_m <- summary(margins(m3, variables=iv_margins, change = "sd")) %>% 
+        mutate(test='Full model', dep_var=dv)
+      margins_out <- bind_rows(margins_out, m3_m)
+      models_out_j[[j]] <- list(m3)
     } 
     models_out[[i]] <- models_out_j %>% purrr::flatten()
   }
@@ -131,13 +168,15 @@ control_names <-c('Baseline', 'Extetended Controls', 'Full Model',
 
 dvs <- c('logDeaths', 'logState_based', 'non_state', 'org3', 'logPpp')
 
+nb_dvs <- c('deaths', 'state_based', 'non_state', 'sqrtorg3')
+
 captions <- c('Deaths (logged)', 'State based conflict events (logged)',
 	      'Non state conflict events', 'Communal violence events', 'PPP
 	      (logged)')
 
 ivs <- c('logSpAll', 'logSpAny')
 
-interactions <- c('SpAllXCapDist', 'SpAnyXCapDist')
+# interactions <- c('SpAllXCapDist', 'SpAnyXCapDist')
 
 #==============================================================================#
 #	Analysis							       #
@@ -147,7 +186,7 @@ linear_models <- GeoISDanalysis(dvs=dvs, ivs=ivs, controls=controls,
 				 data=prio_grid_isd, test_label='Linear
 				 Models')
 
-interaction_models <- GeoISDanalysis(dvs=dvs, ivs=interactions, controls=controls,
+#interaction_models <- GeoISDanalysis(dvs=dvs, ivs=interactions, controls=controls,
 				 data=prio_grid_isd, test_label='Interaction
 				 Models')
 
@@ -182,8 +221,7 @@ full_margins <- linear_models[[1]] %>%
 
 
 margins_main_plot <- ggplot(data = full_margins,
-                            aes(x = factor_label, y = AME, 
-				ymin = lower, ymax = upper, colour=Test)) +
+  aes(x = factor_label, y = AME, ymin = lower, ymax = upper, colour=Test)) +
   facet_wrap(~ facet_label) +
   xlab('') +
   labs(color = 'Models') +
@@ -191,23 +229,75 @@ margins_main_plot <- ggplot(data = full_margins,
   geom_point(position = position_dodge(width = 0.4)) +
   geom_errorbar(position = position_dodge(width = 0.4), width = 0.1) +
   coord_flip() +
- # goldenScatterCAtheme +
   theme(panel.grid.major.y = element_line(colour="grey60", linetype="dashed")) +
   geom_hline(yintercept=0, linetype="dotted") +
   guides(alpha="none") +
-  labs(caption = 
-         "Points are average marginal effects of a 1 SD increase in the independent variable with 95% confidence intervals.")
+  labs(caption = "Points are average marginal effects of a 1 SD increase in the 
+ independent variable with 95% confidence intervals.")
 
-  margins_main_plot
+# TODO: Print to file
+margins_main_plot
 
 # Marginal effects (interaction models)
-fit <- lm(logDeaths ~ logSpAll * capdist + mountains_mean +
-	  water_gc + distcoast + popd + bdist3 + temp_sd + temp + prec_sd +
+fit <- lm(logDeaths ~ logSpAll * logCapdist + mountains_mean +
+	  water_gc + distcoast + logPopd + logBDist + temp_sd + temp + prec_sd +
 	  prec_gpcc + barren_gc + forest_gc, data = prio_grid_isd)
 
-predplot <- ggpredict(fit, terms = c("logSpAll", "capdist"))
+predplot <- ggpredict(fit, terms = c("logSpAll", "logCapdist"))
 
 ggplot(predplot, aes(x, exp(predicted))) +
   geom_line() +
   geom_ribbon(aes(ymin = exp(conf.low), ymax = exp(conf.high)), alpha = .1)
 
+#=============================================================================#
+# Negative binomial models
+
+NB_models <- GeoISD_NBanalysis(dvs=nb_dvs, ivs=ivs, controls=controls,
+				 data=prio_grid_isd, test_label='Negative
+				 binomial Models')
+
+NB_deaths <- glm.nb(deaths ~ logSpAny + logCapdist + mountains_mean +
+ 			water_gc + distcoast + logPopd + logBDist + temp_sd + temp + prec_sd +
+ 			prec_gpcc + barren_gc + forest_gc, data = prio_grid_isd)
+
+NB_state_based <- glm.nb(state_based ~ logSpAny + logCapdist + mountains_mean +
+ 			water_gc + distcoast + logPopd + logBDist + temp_sd + temp + prec_sd +
+ 			prec_gpcc + barren_gc + forest_gc, data = prio_grid_isd)
+
+NB_non_state <- glm.nb(non_state ~ logSpAny + logCapdist + mountains_mean +
+ 			water_gc + distcoast + logPopd + logBDist + temp_sd + temp + prec_sd +
+ 			prec_gpcc + barren_gc + forest_gc, data = prio_grid_isd)
+
+NB_org3 <- glm.nb(sqrtorg3 ~ logSpAny + logCapdist + mountains_mean +
+ 			water_gc + distcoast + logPopd + logBDist + temp_sd + temp + prec_sd +
+ 			prec_gpcc + barren_gc + forest_gc, data = prio_grid_isd)
+
+nb_full_margins <- NB_models[[1]] %>% 
+  mutate(factor_label = case_when(
+    factor == 'logSpAll' ~ 'State Preseance (all)(logged)',
+    factor == 'logSpAny' ~ 'State Preseance (any)(logged)'),
+	facet_label = case_when(
+	dep_var == 'deaths' ~ 'Total fatalities',
+	dep_var == 'state_based' ~ 'State based conflict events',
+	dep_var == 'non_state' ~ 'Non state conflict events',
+	dep_var == 'sqrtorg3' ~ 'Communal violence events (square root
+	transformed)')) %>% 
+	rename(Test=test)
+
+
+margins_nb_plot <- ggplot(data = nb_full_margins,
+  aes(x = factor_label, y = AME, ymin = lower, ymax = upper, colour=Test)) +
+  facet_wrap(~ facet_label) +
+  xlab('') +
+  labs(color = 'Models') +
+  scale_color_manual(values=(brewer.pal(n = 4, name = "Dark2"))) +
+  geom_point(position = position_dodge(width = 0.4)) +
+  geom_errorbar(position = position_dodge(width = 0.4), width = 0.1) +
+  coord_flip() +
+  theme(panel.grid.major.y = element_line(colour="grey60", linetype="dashed")) +
+  geom_hline(yintercept=0, linetype="dotted") +
+  guides(alpha="none") +
+  labs(caption = "Points are average marginal effects of a 1 SD increase in the 
+ independent variable with 95% confidence intervals.")
+
+margins_nb_plot

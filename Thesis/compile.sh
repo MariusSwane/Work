1320d000afae0526f45a58d990db09606634586b
Marius Swane <marius.wishman@ntnu.no>
2022-06-26 23:15:03 +0200

    Sun Jun 26 11:15:02 PM CEST 2022

--- Thesis/compile.sh
+++ Thesis/compile.sh
@@ -3,6 +3,7 @@
 pdflatex main
 bibtex main
 bibtex Kappa/Kappa
+bibtex ARC/main_text_jpr_final
 bibtex StatePresenceCivilConflict/StatePresenceCivilConflict
 bibtex CommunalViolence/OMT
 pdflatex main

dba115fef6855208caf2987f5082029ab7035461
Marius Swane <marius.wishman@ntnu.no>
2023-03-09 19:57:50 +0100

    Thu Mar  9 07:57:50 PM CET 2023

--- Thesis/trialLecture.r
+++ Thesis/trialLecture.r
@@ -1,6 +1,8 @@
 library(countrycode)
 library(dplyr)
 library(ggplot2)
+library(rworldmap)
+library(rworldxtra)
 library(scales)
 library(sf)
 library(spData)
@@ -8,20 +10,26 @@ library(vdemdata)
 
 source('../R/Scripts/goldenScatterCAtheme.r')
 
-vd <- vdem
-
 load("../R/Data/ucdp-prio-acd-211.rdata")
 
 load("../R/Data/GEDEvent_v21_1.RData")
 
-ged <- GEDEvent_v21_1 %>% group_by(country_id) %>% 
+ged <- filter(GEDEvent_v21_1, type_of_violence != 3) %>% group_by(country_id) %>% 
 				   summarise(deaths = sum(best))
 
-data(world)
+worldmap <- getMap(resolution = "high")
 
-world$country_id <- countrycode(world$iso_a2,"iso2c","gwn") 
+worldmap <- st_as_sf(worldmap)
 
-ged <- left_join(world, ged, by = "country_id")
+worldmap$country_id <- countrycode(worldmap$ISO_N3,"iso3n","gwn") 
+
+#st_crs(ged) <- st_crs(worldmap)
+
+ged <- left_join(worldmap, ged, by = "country_id")
+
+ggplot() +
+	geom_sf(data = vd,
+		aes(fill = country_id))
 
 gedplot <- ggplot() +
 	geom_sf(data = ged,
@@ -29,31 +37,45 @@ gedplot <- ggplot() +
 	scale_fill_viridis_c("Fatalities",
     			 labels = trans_format("identity", function(x) x^2)) +
 	theme_minimal() + 
+	theme(text=element_text(size=16,
+        family="Times New Roman")) +
 	goldenScatterCAtheme
 # Preview plot
 gedplot
+
+ggsave("img/gedplot.pdf", gedplot, device = cairo_pdf, width = 10, height = 10/1.68)
+
+vd <- select(vdem, v2x_polyarchy, country_name, e_gdppc)
+
+vd$country_id <- countrycode(vd$country_name,"country.name","gwn") 
+
+vd <- left_join(worldmap, vd, by = "country_id")
+
+vdemplot <- ggplot() +
+	geom_sf(data = vd,
+		aes(fill = v2x_polyarchy)) +
+	scale_fill_viridis_c("Electoral Democracy Index") +
+	theme_minimal() + 
+	theme(text=element_text(size=16,
+        family="Times New Roman")) +
+	goldenScatterCAtheme
+
+# Preview plot
+vdemplot
+
+ggsave("img/vdemplot2.pdf", vdemplot, device = cairo_pdf, width = 10, height = 10/1.68)
 
 gdpplot <- ggplot() +
 	geom_sf(data = ged,
 		aes(fill = sqrt(gdpPercap))) +
 	scale_fill_viridis_c("GDP per capita",
-    			 labels = trans_format("identity", function(x) x^2)) +
+		labels = trans_format("identity", function(x) x^2)) +
 	theme_minimal() + 
+	theme(text=element_text(size=16,
+        family="Times New Roman")) +
 	goldenScatterCAtheme
 # Preview plot
 gdpplot
 
-vdem <- select(vdem, v2x_polyarchy, country_name)
+ggsave("img/gdpplot.pdf", gdpplot, device = cairo_pdf, width = 10, height = 10/1.68)
 
-vdem$country_id <- countrycode(vdem$country_name,"country.name","gwn") 
-
-vdem <- left_join(world, vdem, by = "country_id")
-
-vdemplot <- ggplot() +
-	geom_sf(data = vdem,
-		aes(fill = v2x_polyarchy)) +
-	scale_fill_viridis_c("Electoral Democracy Index")
-	theme_minimal() + 
-	goldenScatterCAtheme
-# Preview plot
-vdemplot
